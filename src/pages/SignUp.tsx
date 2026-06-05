3a22abd25b0304f140623eef536838511849921f
FelipeMASdev <felipe.mas.dev@gmail.com>
2026-06-05 17:31:07 -0300

    add axios and zod dependencies; create api service; implement sign-up form with validation

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,29 @@
 import { useState } from "react";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
+
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+
+const signUpSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: "Informe o nome" }),
+  email: z
+    .email({ message: "Informe um e-mail válido" }),
+  password: z
+    .string()
+    .min(6, { message: "A senha deve conter no mínimo 6 caracteres" }),
+  passwordConfirm: z
+    .string({message: "Confirme a senha"})
+}).refine((data) => data.password === data.passwordConfirm, {
+  message: "As senhas não são iguais",
+  path: ["passwordConfirm"]
+});
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -9,9 +32,43 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
-    console.log({ name, email, password, passwordConfirm });
+
+    try {
+      setIsLoading(true);
+      
+      const data = signUpSchema.parse({ 
+        name, 
+        email, 
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if(confirm("Usuário cadastrado com sucesso! Ir para a tela de login?")) {
+        navigate("/");
+      }
+
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      return alert("Não foi possível cadastrar o usuário!");
+
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
